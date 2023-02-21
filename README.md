fad3e8247364f9b40ad13f52ab000c74f3b02e05
Kipp-IT-Lucas <lucas.pfister@kipp.com>
2023-02-21 14:45:54 +0100

    Init. commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # NetBox-Sync-Framework
+
+Powershell module to integrate your NetBox api in your Powershell sciprts
+
+## Usage
+
+Import the Module and use it!
+
+List of Parametes:
+
+| Name | Description | Required | Deufault value | expectet value | Example value |
+| --- | --- | --- | --- | --- | --- |
+| -Token | Your NetBox api token | [x] | none | [string] | `"xxxxxxxxxxxxxxxxxxxxxx"` |
+| -Uri | The url to the NetBox instance (with scheme) | [x] | none | [string] | `"https://netbox.example.com"` |
+| -Body | Body data for the NetBox (formatet as powershell object or array. For more information see the NetBox Documentation) | [ ] | none | [object] | `@{'address' = '10.0.0.5/24', 'status' = 'active'}` |
+| -Query | Query data for the NetBox | [ ] | none | [string] | `limit=10` |
+| -Create | Creates the called object (endpoints listed in the netbox documentation without a leading slash) | [ ]* | none | [string] | `ipam/ip-addresses` |
+| -Delete | Delete the called object (endpoints listed in the netbox documentation without a leading slash) | [ ]* | none | [string] | `ipam/ip-addresses` |
+| -PartialUpdate | PartialUpdate the called object (endpoints listed in the netbox documentation without a leading slash) | [ ]* | [string] | none | `ipam/ip-addresses` |
+| -BulkUpdate | BulkUpdate the called object (endpoints listed in the netbox documentation without a leading slash) | [ ]* | none | [string] | `ipam/ip-addresses` |
+| -Select | Select the called object (endpoints listed in the netbox documentation without a leading slash) | [ ]* | none | [bool] | `ipam/ip-addresses` |
+| -Debug | enables debug | [ ] | $false | `$true` |
---
+++ netbox-api.ps1
@@ -0,0 +1,66 @@
+function Netbox {
+    param (
+        [string]$Token,
+        [string]$Uri,
+        [object]$Body,
+        [string]$Query,
+        [string]$Create = -1,
+        [string]$Delete = -1,
+        [string]$PartialUpdate = -1,
+        [string]$BulkUpdate = -1,
+        [string]$Select = -1,
+        [bool]$Debug = $false
+    )
+
+    #Check for required variables
+    if (!$Token -or !$Uri) {
+        Write-Output "You need to specifiy the Netbox Token and the URL."
+        Exit-PSHostProcess
+    }
+    
+    #detect http method to use
+    $method = -1
+    
+    if ($Create -ne -1) {
+        $method = "Post"
+        $task = $Create
+    }elseif ($Delete -ne -1) {
+        $method = "Delete"
+        $task = $Delete
+    }elseif ($PartialUpdate -ne -1) {
+        $method = "Patch"
+        $task = $PartialUpdate
+    }elseif ($BulkUpdate -ne -1) {
+        $method = "Put"
+        $task = $BulkUpdate
+    }elseif ($Select -ne -1) {
+        $method = "Get"
+        $task = $Select
+    }
+
+    if ($method -eq -1) {
+        Write-Output "Please you need to specify one of these options: -Create, -Delete, -PartialUpdate, -BulkUpdate. For more informations see the Documentation."
+        Exit-PSHostProcess
+    }
+
+    #create request url
+    if ($Query) {
+        $url = "$Uri/api/$task/?$Query"
+    }else{
+        $url = "$Uri/api/$task/"
+    }
+
+    if ($Debug -eq $true) {
+        Write-Output "$method URL $url"
+
+        if ($Body) {
+            Write-Output "Body: " $Body
+        }
+    }
+
+    $res = Invoke-WebRequest -Headers @{'Authorization' = 'Token ' + $Token; 'Accept' = 'application/json'; 'indent' = '4'} -ContentType 'application/json' -Method $method -UseBasicParsing -Uri $url -Body $($Body | ConvertTo-Json -Depth 64)
+
+    if ($res.StatusCode -gt 199 -and $res.StatusCode -lt 300) {
+        return $($res.Content | ConvertFrom-Json).results
+    }
+}
